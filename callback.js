9e6ad7a1aa329823ab540aa295a9aba60348b082
salvatierraggg <salvatierraggg@gmail.com>
2020-03-03 20:49:22 -0300

    no veo el pizarron sin los anteojos :c

--- callback.js
+++ callback.js
@@ -1,7 +1,31 @@
-function siguiente(x, done) {
-  done(x + 1)
+function siguiente(x, next) {
+  next(x + 1)
 }
 
-siguiente(2, resultado => { 
-  console.log('Siguiente de 8 =>', resultado)
+function doble(x,next){
+  next(x*2)
+}
+
+function siguienteDelDoble(x,next){
+  next(siguiente(x,doble/*(x,???)*/)) 
+}
+
+function dobleDelSiguiente(x,next){
+  siguiente(x,doble(x,next))
+}
+
+
+siguiente(8,resultado=>{
+  console.log("siguiente de 8 =>",resultado)
 })
+
+doble(8,resultado=>{
+  console.log("doble de 8 =>",resultado)
+})
+siguienteDelDoble(8,resultado=>{
+  console.log("siguienteDelDoble de 8 =>",resultado)
+})
+
+/*dobleDelSiguiente(8,resultado=>{
+  console.log("dobleDelSiguiente de 8 =>",resultado)
+})*/
